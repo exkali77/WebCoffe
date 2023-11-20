36d3d6afe1298b68751103bfeb79cd501dbf88b3
Kerem Şenses <68199514+exkali77@users.noreply.github.com>
2023-11-20 15:07:25 +0300

    Add project files.

---
+++ WebCoffe/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCoffe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
